fe9ca1b7392a9ecf8d1343b4b95af9aa05c78859
Alexander <allanosinhb@gmail.com>
2023-10-21 10:10:47 -0700

    6 passing

--- array-practice.js
+++ array-practice.js
@@ -1,38 +1,72 @@
-const findMinimum = arr => {
-
-  // Your code here 
-
-};
-
-const runningSum = arr => {
-
-  // Your code here 
-};
-
-const evenNumOfChars = arr => {
-
-  // Your code here 
-};
-
-const smallerThanCurr = arr => {
-
-  // Your code here 
+const findMinimum = arr => { // time o(n), space o(1)
+  if (!arr.length) {
+    return undefined
+  }
+  return Math.min(...arr)
 
 };
 
-const twoSum = (arr, target) => {
-
-  // Your code here 
+const runningSum = arr => { //time o(n^2), space o(n2)
+  if (!arr.length) {
+    return arr
+  }
+  let newArr = []
+  let copyArr = arr.slice()
+  let total = copyArr.reduce((acc, num) => {
+    return acc + num
+  })
+  newArr.push(total)
+  arr.pop()
+  return runningSum(arr).concat(newArr)
 };
 
-const secondLargest = arr => {
+const evenNumOfChars = arr => { //time O(N), space O(1)
+  let counter = 0
+  arr.forEach(string => {
+    if (string.length % 2 === 0) {
+      counter++
+    }
+  })
+  return counter
+};
 
-  // Your code here 
+const smallerThanCurr = arr => { //time O(N^2), space O(n)
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    let counter = 0
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        counter ++
+      }
+    }
+    newArr.push(counter)
+  }
+  return newArr
+};
+
+const twoSum = (arr, target) => { //time O(n^2), space O(1)
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return true
+      }
+    }
+  }
+  return false
+};
+
+const secondLargest = arr => { //time O(n^2),
+  if (arr.length < 2) {
+    return undefined
+  }
+  let index = arr.indexOf(Math.max(...arr))
+  arr.splice(index, 1)
+  return Math.max(...arr)
 };
 
 const shuffle = (arr) => {
 
-  // Your code here 
+  // Your code here
 };
 
 
